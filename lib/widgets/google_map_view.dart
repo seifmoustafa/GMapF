025cf92428ffae89b6b2436d2d5406ae8fde19ce
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-11-15 19:28:41 +0200

    "Added LocationService import and functionality to GoogleMapView to update camera position to current location"

--- lib/widgets/google_map_view.dart
+++ lib/widgets/google_map_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gmapf/utils/location_service.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class GoogleMapView extends StatefulWidget {
@@ -10,20 +11,39 @@ class GoogleMapView extends StatefulWidget {
 
 class _GoogleMapViewState extends State<GoogleMapView> {
   late CameraPosition initialCameraPositin;
+  late GoogleMapController googleMapController;
+  late LocationService locationService;
   @override
   void initState() {
     initialCameraPositin = const CameraPosition(
-      zoom: 17,
-      target: LatLng(29.97373651037227, 31.234217425881088),
+      target: LatLng(0, 0),
     );
+    locationService = LocationService();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
+      onMapCreated: (controller) {
+        googleMapController = controller;
+        updateCurrentLocation();
+      },
       initialCameraPosition: initialCameraPositin,
       zoomControlsEnabled: false,
     );
+  }
+
+  void updateCurrentLocation() async {
+    try {
+      var locationData = await locationService.getLocation();
+      CameraPosition myCurrentCameraPosition = CameraPosition(
+          zoom: 16,
+          target: LatLng(locationData.latitude!, locationData.longitude!));
+      googleMapController.animateCamera(
+          CameraUpdate.newCameraPosition(myCurrentCameraPosition));
+    } on LocationServiceException catch (e) {
+    } on LocationPermissionException catch (e) {
+    } catch (e) {}
   }
 }
