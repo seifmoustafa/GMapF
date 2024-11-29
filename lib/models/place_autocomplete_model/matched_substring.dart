faecc14535ee2d6808c662a6228c75d38bd0b583
Seif Moustafa <58215490+seifmoustafa@users.noreply.github.com>
2024-11-29 15:07:45 +0200

    "Formatting changes to models in place autocomplete model: whitespace and indentation adjustments in MainTextMatchedSubstring, MatchedSubstring, StructuredFormatting, and Term classes."

--- lib/models/place_autocomplete_model/matched_substring.dart
+++ lib/models/place_autocomplete_model/matched_substring.dart
@@ -1,20 +1,18 @@
 class MatchedSubstring {
-	int? length;
-	int? offset;
+  int? length;
+  int? offset;
 
-	MatchedSubstring({this.length, this.offset});
+  MatchedSubstring({this.length, this.offset});
 
-	factory MatchedSubstring.fromJson(Map<String, dynamic> json) {
-		return MatchedSubstring(
-			length: json['length'] as int?,
-			offset: json['offset'] as int?,
-		);
-	}
+  factory MatchedSubstring.fromJson(Map<String, dynamic> json) {
+    return MatchedSubstring(
+      length: json['length'] as int?,
+      offset: json['offset'] as int?,
+    );
+  }
 
-
-
-	Map<String, dynamic> toJson() => {
-				'length': length,
-				'offset': offset,
-			};
+  Map<String, dynamic> toJson() => {
+        'length': length,
+        'offset': offset,
+      };
 }
